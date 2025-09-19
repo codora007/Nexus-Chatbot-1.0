103981e6282f654085f98f2e79fc5bcc643ebeb6
Akshay <akshaybala2007@gmail.com>
2025-09-19 12:26:28 +0530

    Add files via upload

---
+++ fixed responses chatbot.py
@@ -0,0 +1,15 @@
+Python 3.13.0 (tags/v3.13.0:60403a5, Oct  7 2024, 09:38:07) [MSC v.1941 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+= RESTART: C:/Users/online.SCAS/Downloads/Akshay Bala/Github Projects/Chatbot/chatbot.py
+Hi i am a simple chatbot, Type 'bye' to exit.
+You:hi
+Bot:Hello!
+You:how are you
+Bot: I'm fine, thanks for asking and its been good
+You:what is your name?
+Bot: Sorry i did not understand.
+You:who are you
+Bot: I am Evela.
+You:bye
+Bot: Goodbye my friend.
